c57b8576f4731b7e1b628fe6572602363c9cc760
joker-eph <joker-eph@users.noreply.github.com>
2020-01-31 16:23:09 +0000

    Automated deployment: Fri Jan 31 16:23:09 UTC 2020 0aa5666d17cc7084cbc81d83f029cfdb37a10b19

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -293,6 +293,7 @@ var menudata={children:[
 {text:"Enumerations",url:"globals_enum.html"},
 {text:"Enumerator",url:"globals_eval.html"},
 {text:"Macros",url:"globals_defs.html",children:[
+{text:"c",url:"globals_defs.html#index_c"},
 {text:"d",url:"globals_defs.html#index_d"},
 {text:"g",url:"globals_defs.html#index_g"},
 {text:"i",url:"globals_defs.html#index_i"},
